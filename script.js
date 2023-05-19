68f2ce3ac0a7b21ba637ebb4b6455dab6bfabe65
MichaelMees99 <120432979+MichaelMees99@users.noreply.github.com>
2023-05-19 02:07:41 -0400

    initial commit
    base to build upon soon, nothing is functional yet

---
+++ script.js
@@ -0,0 +1,4 @@
+//Open weather api key 17c280793daafa3260db5fc3c6e289a0
+//main API https://api.openweathermap.org/data/2.5/forecast?lat={lat}&lon={lon}&appid={API key}
+
+//geocode to get lat lon: http://api.openweathermap.org/geo/1.0/direct?q={city name},{state code},{country code}&limit={limit}&appid={API key}
